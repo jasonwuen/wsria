56abaf4891fdb9666557ae0ddb62d707ac0e586d
yanhonglei <yanhonglei@b4ea3878-8aa3-6609-8a57-222449c04924>
2010-10-03 12:42:46 +0000

    添加文件下载Action

---
+++ trunk/wsria-demo/src/main/java/cn/wsria/util/BrowserUtils.java
@@ -0,0 +1,54 @@
+package cn.wsria.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 和浏览器有关的工具类
+ *
+ * @author HenryYan
+ *
+ */
+public class BrowserUtils {
+
+	/**
+	 * 判断是否为IE浏览器
+	 */
+	public static boolean isIE(HttpServletRequest request) {
+		String agent = getAgent(request);
+		if (null != agent && -1 != agent.indexOf("MSIE")) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 判断是否为FireFox浏览器
+	 */
+	public static boolean isMozilla(HttpServletRequest request) {
+		String agent = getAgent(request);
+		if (null != agent && -1 != agent.indexOf("Mozilla")) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 是否为Chrome或者Safari
+	 */
+	public static boolean isWebKit(HttpServletRequest request) {
+		String agent = getAgent(request);
+		if (null != agent && -1 != agent.indexOf("WebKit")) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 获取浏览器USER-AGENT
+	 */
+	public static String getAgent(HttpServletRequest request) {
+		String agent = request.getHeader("USER-AGENT");
+		return agent;
+	}
+
+}
