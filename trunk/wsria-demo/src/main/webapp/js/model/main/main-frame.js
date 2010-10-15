912418ecbd8515be916574f3071e4e22a912a279
yanhonglei <yanhonglei@b4ea3878-8aa3-6609-8a57-222449c04924>
2010-10-15 17:16:51 +0000

    pmd添加地区和数据字典的设计

--- trunk/wsria-demo/src/main/webapp/js/model/main/main-frame.js
+++ trunk/wsria-demo/src/main/webapp/js/model/main/main-frame.js
@@ -14,10 +14,15 @@ var _menus = {
 		"icon" : "icon-sys",
 		"menuname" : "地区信息",
 		"menus" : [ {
-			"menuid" : "11",
+			"menuid" : "21",
+			"menuname" : "area2select插件简介",
+			"icon" : "icon-nav",
+			"url" : "../demo/area/area-plugin-caption.action"
+		}, {
+			"menuid" : "22",
 			"menuname" : "动态加载地区下拉框",
 			"icon" : "icon-nav",
-			"url" : "easyuiCtrl/Accordion.html"
+			"url" : "../demo/area/area.action"
 		} ]
 	}, {
 		"menuid" : "9999",
