c4819aafa2072cfd1dd665096e178ffd06c3ff83
yanhonglei <yanhonglei@b4ea3878-8aa3-6609-8a57-222449c04924>
2010-11-05 11:36:17 +0000

    [maven-scm] copy for tag wsria-demo-sso-1.0.0

---
+++ src/main/java/com/wsria/util/constant/ServerConstants.java
@@ -0,0 +1,16 @@
+package com.wsria.util.constant;
+
+/**
+ * 德农积分银行系统常量
+ *
+ * @author HenryYan
+ *
+ */
+public class ServerConstants {
+	
+	/**
+	 *	系统内部错误中文提示
+	 */
+	public static final String RESPONSE_SYSTEM_ERROR = "抱歉，系统内部错误！";
+	
+}
